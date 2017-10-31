7e0db68056c498945e66d7280da5b7da234dd6c3
Tom Chan <tomchanthree@gmail.com>
2017-10-31 02:35:45 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -28,7 +28,7 @@ def won?(board)
       if position_1 == "X" && position_2 == "X" && position_3 == "X"
         return array # return the win_combination indexes that won.
       elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-        return array 
+        return array
       else
         false
       end
