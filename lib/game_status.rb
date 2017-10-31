5dfd628490f647511d14c4314a37688b3e2244c9
Tom Chan <tomchanthree@gmail.com>
2017-10-31 02:25:41 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,7 +26,7 @@ def won?(board)
       position_3 = board[win_index_3] # load the value of the board at win_index_3
 
       if position_1 == "X" && position_2 == "X" && position_3 == "X"
-        return cell# return the win_combination indexes that won.
+        return array# return the win_combination indexes that won.
       else
         false
       end
